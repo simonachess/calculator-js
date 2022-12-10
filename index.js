5e30a0908d379ec5f8ed7a2ec5e542b88041dc49
simonachess <simonachess@gmail.com>
2022-12-10 10:21:49 +0200

    update

--- index.js
+++ index.js
@@ -1,79 +1,85 @@
-let isActionPressed = false;
-let isEqualsPressed = false;
-let resultRow = document.getElementById('result');
-resultRow.innerHTML = '0';
+let isActionPressed = false
+let isEqualsPressed = false
+let resultRow = document.getElementById('result')
+resultRow.innerHTML = '0'
 
-let actionRow = document.getElementById('actions');
+let actionRow = document.getElementById('actions')
 
 function numberPress(number) {
     if (isEqualsPressed) {
-        actionRow.innerHTML = '';
-        resultRow.innerHTML = number;
-        isEqualsPressed = false;
+        actionRow.innerHTML = ''
+        resultRow.innerHTML = number
+        isEqualsPressed = false
     } else if (isActionPressed) {
-        resultRow.innerHTML = number;
-        isActionPressed = false;
+        resultRow.innerHTML = number
+        isActionPressed = false
     } else if (resultRow.innerHTML === '0') {
-        resultRow.innerHTML = number;
+        resultRow.innerHTML = number
     } else {
-        resultRow.innerHTML = resultRow.innerHTML + number;
+        resultRow.innerHTML = resultRow.innerHTML + number
     }
 }
 
 function erase() {
-    resultRow.innerHTML = '0';
-    actionRow.innerHTML = '';
+    resultRow.innerHTML = '0'
+    actionRow.innerHTML = ''
 }
 function eraseLastNumber() {
     if (resultRow.innerHTML.length === 1) {
-        resultRow.innerHTML = '0';
+        resultRow.innerHTML = '0'
     } else {
-        resultRow.innerHTML = resultRow.innerHTML.substring(0, resultRow.innerHTML.length - 1);
+        resultRow.innerHTML = resultRow.innerHTML.substring(0, resultRow.innerHTML.length - 1)
     }
 }
 
 function actionPress(action) {
-    isActionPressed = true;
-    isEqualsPressed = false;
+    isActionPressed = true
+    isEqualsPressed = false
 
-    let numberAndAction = actionRow.innerHTML.split(' ');
+    let numberAndAction = actionRow.innerHTML.split(' ')
 
     if (numberAndAction.length === 2) {
         if (numberAndAction[1] === '+') {
-            resultRow.innerHTML = Number(numberAndAction[0]) + Number(resultRow.innerHTML);
+            resultRow.innerHTML = Number(numberAndAction[0]) + Number(resultRow.innerHTML)
         } else if (numberAndAction[1] === '-') {
-            resultRow.innerHTML = Number(numberAndAction[0]) - Number(resultRow.innerHTML);
+            resultRow.innerHTML = Number(numberAndAction[0]) - Number(resultRow.innerHTML)
         } else if (numberAndAction[1] === 'x') {
-            resultRow.innerHTML = Number(numberAndAction[0]) * Number(resultRow.innerHTML);
+            resultRow.innerHTML = Number(numberAndAction[0]) * Number(resultRow.innerHTML)
         } else if (numberAndAction[1] === '/') {
-            resultRow.innerHTML = Number(numberAndAction[0]) / Number(resultRow.innerHTML);
+            resultRow.innerHTML = Number(numberAndAction[0]) / Number(resultRow.innerHTML)
         }
 
     }
 
-    actionRow.innerHTML = resultRow.innerHTML + ' ' + action;
+    actionRow.innerHTML = resultRow.innerHTML + ' ' + action
 }
 
 function equals() {
-    let numberAndAction = actionRow.innerHTML.split(' ');
+    let numberAndAction = actionRow.innerHTML.split(' ')
 
     actionRow.innerHTML = actionRow.innerHTML + ' ' + resultRow.innerHTML + ' ='
 
     if (numberAndAction[1] === '+') {
-        resultRow.innerHTML = Number(numberAndAction[0]) + Number(resultRow.innerHTML);
+        resultRow.innerHTML = Number(numberAndAction[0]) + Number(resultRow.innerHTML)
     } else if (numberAndAction[1] === '-') {
-        resultRow.innerHTML = Number(numberAndAction[0]) - Number(resultRow.innerHTML);
+        resultRow.innerHTML = Number(numberAndAction[0]) - Number(resultRow.innerHTML)
     } else if (numberAndAction[1] === 'x') {
-        resultRow.innerHTML = Number(numberAndAction[0]) * Number(resultRow.innerHTML);
+        resultRow.innerHTML = Number(numberAndAction[0]) * Number(resultRow.innerHTML)
     } else if (numberAndAction[1] === '/') {
-        resultRow.innerHTML = Number(numberAndAction[0]) / Number(resultRow.innerHTML);
+        if ( Number(numberAndAction[0]) / Number(resultRow.innerHTML) === Infinity) {
+            alert('Action is not possible')
+            resultRow.innerHTML = '0'
+            actionRow.innerHTML = ''
+            return
+        }
+        resultRow.innerHTML = Number(numberAndAction[0]) / Number(resultRow.innerHTML)
     }
 
-    isEqualsPressed = true;
+    isEqualsPressed = true
 }
 
 function dotPress() {
     if (resultRow.innerHTML.indexOf('.') === -1) {
-        resultRow.innerHTML = resultRow.innerHTML + '.';
+        resultRow.innerHTML = resultRow.innerHTML + '.'
     }
 }
